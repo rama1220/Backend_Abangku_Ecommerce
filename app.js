6cbb3b405ec107ca6f439fbc2d5cd780a305dc43
Muchammad Ramadhan <73187710+rama1220@users.noreply.github.com>
2024-03-13 03:48:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://abangku-ecommerce.vercel.app", "http://127.0.0.1:5173"],
+    origin: ["https://abangku-ecommerce.vercel.app"],
     credentials: true,
   })
 );
