f1b8450c5cd0fdfe6e549944d48daa855aa5123b
Muchammad Ramadhan <73187710+rama1220@users.noreply.github.com>
2024-03-12 20:03:34 +0700

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ app.use(
 );
 
 app.use(express.json());
+
+// Middleware untuk menambahkan header CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://abangku-ecommerce.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(router);
 
 export default app;
