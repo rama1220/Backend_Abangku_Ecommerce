a104a606990881d33d8b0c2c53c3cbed5cbcdfc4
Muchammad Ramadhan <73187710+rama1220@users.noreply.github.com>
2024-03-13 12:33:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
   next();
 });
-app.options("*", cors());
+app.options("https://abangku-ecommerce.vercel.app", cors());
 app.use(express.json());
 app.use(router);
 
